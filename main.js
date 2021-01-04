b84c79c2c85c0e2b60e5bb2b8464998f0b627c0c
MUNGERI Frank <54407341+mfrank37@users.noreply.github.com>
2021-01-04 09:51:03 +0200

    Connect to the Server backend (#2)
    * add hover background for chatlist
    
    * ch: add and setup socket io

---
+++ main.js
@@ -0,0 +1,43 @@
+let TOKEN = 'visitor'; // for testing
+// TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiZWZhY2U0M2MtMmU0OS00NzNiLWJiZTItMzA1ZDFhNTE5MGYxIiwidXNlcm5hbWUiOiJtZnJhbmszNyIsImlhdCI6MTYwOTUxOTcwMCwiZXhwIjoxNjEwMTI0NTAwfQ.b_gRE91TZAaTSTSkwGp798_KOq9PP6kbe7FIePvGQ6A';
+
+const socket = io('http://localhost:3000');
+
+socket.on('chat-message', data => {
+    console.log(data);
+});
+
+socket.on('new-connection' , msg => {
+    console.log(msg);
+});
+
+socket.on('disconnected', msg => {
+    console.log(msg);
+});
+
+const txtArea = document.querySelector('textarea');
+
+
+const sendMessage = (evt) => {
+    evt.preventDefault();
+    let message = txtArea.value;
+    txtArea.value = '';
+    txtArea.focus();
+    if(!message) return;
+    socket.emit('chat-private', sendIt(message, TOKEN));
+    let sentMsg = document.createElement('div');
+    sentMsg.classList.add('sent');
+    sentMsg.innerHTML = message;
+    document.querySelector('.messages').appendChild(sentMsg);
+}
+
+const sendIt = (message, TOKEN='visitor') => {
+    return {
+        token:TOKEN,
+        message: message,
+        time : new Date()
+    }
+}
+
+const form = document.querySelector('form.send');
+form.addEventListener('submit', sendMessage);
